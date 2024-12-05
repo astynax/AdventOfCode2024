4e65a3c5f9a648aee162db274c0ab172509eccf1
astynax <astynax@users.noreply.github.com>
2024-12-05 17:05:31 +0400

    add the scaffolding machinery

---
+++ templates/day.clj
@@ -0,0 +1,8 @@
+(ns advent-of-code-2024.day-{{ day }}
+  (:require [clojure.java.io :as io]))
+
+(def input (slurp (io/resource "day-{{ day }}.input")))
+
+(def solution1 :TODO)
+
+(def solution2 :TODO)
---
+++ templates/test.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2024.day-{{ day }}-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code-2024.day-{{ day }} :refer :all]))
+
+(deftest solution1-test
+  (is (= :TODO solution1)))
+
+(deftest solution2-test
+  (is (= :TODO solution2)))
