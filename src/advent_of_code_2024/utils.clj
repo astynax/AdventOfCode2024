8c281ac4333cc294a0c69673990070e2d12e8c30
astynax <astynax@users.noreply.github.com>
2024-12-06 17:58:53 +0400

    add the solution for the day 06

--- src/advent_of_code_2024/utils.clj
+++ src/advent_of_code_2024/utils.clj
@@ -1,4 +1,5 @@
-(ns advent-of-code-2024.utils)
+(ns advent-of-code-2024.utils
+  (:require [clojure.string :as s]))
 
 (defn enumerate [xs]
   (map vector (iterate inc 0) xs))
@@ -8,3 +9,16 @@
 (defn transpose [xss]
   (mapv (fn [i] (mapv #(get % i) xss))
         (range (count (first xss)))))
+
+(defn topo-map
+  ([text] (topo-map text (fn [_ v] v)))
+  ([text look-at]
+   (reduce
+    (fn [m [k v]] (assoc m k v))
+    {}
+    (for [[y row] (enumerate (s/split-lines (s/trim text)))
+          [x chr] (enumerate row)
+          :let [pos [x y]
+                v (look-at pos chr)]
+          :when (some? v)]
+      [pos v]))))
