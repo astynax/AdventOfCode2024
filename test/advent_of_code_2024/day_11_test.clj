97eea434885c59cf07b0bf159e773f56ecc7bac6
astynax <astynax@users.noreply.github.com>
2024-12-11 14:45:42 +0400

    add the solution for the day 11

---
+++ test/advent_of_code_2024/day_11_test.clj
@@ -0,0 +1,20 @@
+(ns advent-of-code-2024.day-11-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code-2024.day-11 :refer :all]))
+
+(deftest split-stone-test
+  (are [s e] (= e (split-stone s))
+    1234   [12 34]
+    12     [1 2]
+    123    nil
+    123456 [123 456]
+    123000 [123 0]))
+
+(deftest count-after-blinks-test
+  (is (= 55312 (count-after-blinks 25 [125 17]))))
+
+(deftest solution1-test
+  (is (= 186424 solution1)))
+
+(deftest solution2-test
+  (is (= 219838428124832 (solution2))))
