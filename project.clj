0a525c4fc5d054c60cf7daffdaf21af66228f9cc
astynax <astynax@users.noreply.github.com>
2024-12-16 21:27:55 +0400

    add the solution for the day 16

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/astynax/AdventOfCode2024"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.12.0"]]
-  :repl-options {:init-ns advent-of-code-2024.core}
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/data.priority-map "1.2.0"]
+                 [aysylu/loom "1.0.2"]]
   :jvm-opts ["-Djdk.attach.allowAttachSelf"])
